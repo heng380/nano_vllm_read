9c6c1382259543c0990787a8046f73cde1806192
hengcao <hengcao@microsoft.com>
2025-12-04 20:10:47 +0800

    add remarks

--- nanovllm/engine/block_manager.py
+++ nanovllm/engine/block_manager.py
@@ -5,7 +5,7 @@ import numpy as np
 from nanovllm.engine.sequence import Sequence
 
 
-class Block:
+class Block:    # 一个block就是一个定长seq
 
     def __init__(self, block_id):
         self.block_id = block_id
@@ -25,30 +25,30 @@ class Block:
 
 class BlockManager:
 
-    def __init__(self, num_blocks: int, block_size: int):
-        self.block_size = block_size
-        self.blocks: list[Block] = [Block(i) for i in range(num_blocks)]
-        self.hash_to_block_id: dict[int, int] = dict()
-        self.free_block_ids: deque[int] = deque(range(num_blocks))
-        self.used_block_ids: set[int] = set()
+    def __init__(self, num_blocks: int, block_size: int):  # num_blocks是通过utilization计算出给kv cache的显存之后,除以每个block需要的显存大小,算出来的最大block数量
+        self.block_size = block_size     
+        self.blocks: list[Block] = [Block(i) for i in range(num_blocks)]     # 所有block
+        self.hash_to_block_id: dict[int, int] = dict()     # 通过hash值来定位是否已经存在相同的block
+        self.free_block_ids: deque[int] = deque(range(num_blocks))        # 双端队列
+        self.used_block_ids: set[int] = set()     # set, 快速定位已经使用过的block
 
     @classmethod
-    def compute_hash(cls, token_ids: list[int], prefix: int = -1):
+    def compute_hash(cls, token_ids: list[int], prefix: int = -1):   # 依赖上一个hash的累计hash值
         h = xxhash.xxh64()
         if prefix != -1:
             h.update(prefix.to_bytes(8, "little"))
         h.update(np.array(token_ids).tobytes())
         return h.intdigest()
 
-    def _allocate_block(self, block_id: int) -> Block:
+    def _allocate_block(self, block_id: int) -> Block:  # 从free的双端队列中获取头部的block id
         block = self.blocks[block_id]
-        assert block.ref_count == 0
+        assert block.ref_count == 0   # 没有ref
         block.reset()
         self.free_block_ids.remove(block_id)
         self.used_block_ids.add(block_id)
-        return self.blocks[block_id]
+        return self.blocks[block_id]   # 分配的block结构体
 
-    def _deallocate_block(self, block_id: int) -> Block:
+    def _deallocate_block(self, block_id: int) -> Block:  # 
         assert self.blocks[block_id].ref_count == 0
         self.used_block_ids.remove(block_id)
         self.free_block_ids.append(block_id)
@@ -56,32 +56,32 @@ class BlockManager:
     def can_allocate(self, seq: Sequence) -> bool:
         return len(self.free_block_ids) >= seq.num_blocks
 
-    def allocate(self, seq: Sequence):
-        assert not seq.block_table
+    def allocate(self, seq: Sequence):    #只在prefill的时候调用一次, 给输入的sequence一次性分配所有block
+        assert not seq.block_table     # 尚未分配block
         h = -1
         cache_miss = False
-        for i in range(seq.num_blocks):
+        for i in range(seq.num_blocks):    # num_blocks是token长度除以block_size + 1, 向上取整
             token_ids = seq.block(i)
-            h = self.compute_hash(token_ids, h) if len(token_ids) == self.block_size else -1
+            h = self.compute_hash(token_ids, h) if len(token_ids) == self.block_size else -1   # 对满的block计算hash
             block_id = self.hash_to_block_id.get(h, -1)
-            if block_id == -1 or self.blocks[block_id].token_ids != token_ids:
+            if block_id == -1 or self.blocks[block_id].token_ids != token_ids:  # cache miss
                 cache_miss = True
-            if cache_miss:
+            if cache_miss:    # 一个cache miss之后, 后面的就算一样也当作cache miss来处理
                 block_id = self.free_block_ids[0]
                 block = self._allocate_block(block_id)
-            else:
-                seq.num_cached_tokens += self.block_size
-                if block_id in self.used_block_ids:
+            else:    # cache找到了
+                seq.num_cached_tokens += self.block_size   # 计数总共的cached的token
+                if block_id in self.used_block_ids:   # 找到对应的block, ref_count累加
                     block = self.blocks[block_id]
                     block.ref_count += 1
                 else:
-                    block = self._allocate_block(block_id)
-            if h != -1:
+                    block = self._allocate_block(block_id)    # 分配一块新的内存
+            if h != -1:     # block是满的
                 block.update(h, token_ids)
                 self.hash_to_block_id[h] = block_id
             seq.block_table.append(block_id)
 
-    def deallocate(self, seq: Sequence):
+    def deallocate(self, seq: Sequence):    # 后面的block更容易是特有的, 所以先检查, 先释放
         for block_id in reversed(seq.block_table):
             block = self.blocks[block_id]
             block.ref_count -= 1
--- nanovllm/engine/sequence.py
+++ nanovllm/engine/sequence.py
@@ -11,27 +11,27 @@ class SequenceStatus(Enum):
     FINISHED = auto()
 
 
-class Sequence:
+class Sequence:         # 本质上是一个list[int]的token_ids的句子, 增加了一些接口, 变量
     block_size = 256
     counter = count()
 
     def __init__(self, token_ids: list[int], sampling_params = SamplingParams()):
-        self.seq_id = next(Sequence.counter)
-        self.status = SequenceStatus.WAITING
-        self.token_ids = copy(token_ids)
+        self.seq_id = next(Sequence.counter)    # 句子数量编号
+        self.status = SequenceStatus.WAITING    
+        self.token_ids = copy(token_ids)        
         self.last_token = token_ids[-1]
-        self.num_tokens = len(self.token_ids)
-        self.num_prompt_tokens = len(token_ids)
+        self.num_tokens = len(self.token_ids)  # 动态的
+        self.num_prompt_tokens = len(token_ids)  # 静态的
         self.num_cached_tokens = 0
-        self.block_table = []
+        self.block_table = []     # 当前sequence属于哪几个block
         self.temperature = sampling_params.temperature
         self.max_tokens = sampling_params.max_tokens
         self.ignore_eos = sampling_params.ignore_eos
 
     def __len__(self):
-        return self.num_tokens
+        return self.num_tokens   # 重载len(seq)运算符
 
-    def __getitem__(self, key):
+    def __getitem__(self, key):    # 重载seq[i] 运算符
         return self.token_ids[key]
 
     @property
@@ -51,11 +51,11 @@ class Sequence:
         return self.token_ids[self.num_prompt_tokens:]
 
     @property
-    def num_cached_blocks(self):
+    def num_cached_blocks(self):        # 需要几个 kv cache block来装kv cache
         return self.num_cached_tokens // self.block_size
 
     @property
-    def num_blocks(self):
+    def num_blocks(self):  # 向上取整
         return (self.num_tokens + self.block_size - 1) // self.block_size
 
     @property
@@ -66,7 +66,7 @@ class Sequence:
         assert 0 <= i < self.num_blocks
         return self.token_ids[i*self.block_size: (i+1)*self.block_size]
 
-    def append_token(self, token_id: int):
+    def append_token(self, token_id: int):   # 增加一个token, prefill/decode出来的
         self.token_ids.append(token_id)
         self.last_token = token_id
         self.num_tokens += 1
